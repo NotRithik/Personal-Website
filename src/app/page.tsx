f593e4305cec81b2381d9c6c851a189a0c985146
Rithik Kumar S <rithikum123@gmail.com>
2025-02-28 03:45:03 +0530

    Landing page checkpoint 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,29 @@ export default function Home() {
   return (
     <div className="h-screen w-full grid-overlap-container">
       <CustomCursor />
-      <div className="custom-cursor-reveal-layer h-full w-full z-20 grid-overlap-container grid-overlap-element">
-        <BlurryMovingBlobs className="bg-darkest-purple grid-overlap-element"/>
-        <div className="h-screen w-full px-[15%] grid-overlap-element">
+      <div className="custom-cursor-reveal-layer h-full w-full grid-overlap-container grid-overlap-element z-50">
+        <BlurryMovingBlobs className="bg-darkest-purple grid-overlap-element" />
+        <div className="h-screen w-full px-[9%] py-[7.5%] grid-overlap-element landing-page-content-grid z-51">
           hi from masked overlapper
         </div>
       </div>
-      <div className="main-content-layer h-full w-full z-10 px-[15%] grid-overlap-element">
-        <SocialMediaLinks />
-        hi from non-overlapper!
+      <div className="select-none main-content-layer h-full w-full z-10 px-[9%] py-[3%] grid-overlap-element landing-page-content-grid bg-black text-white">
+        <p className="area-social-media">hi from non-overlapper!</p>
+
+        <p className="area-navbar">hi from navbar</p>
+
+        <div className="area-content-1 flex flex-row items-center px-[10%] justify-between">
+          <img src="/images/LandingPic1.png" className="max-h-[35vh] rounded-2xl" />
+          <div className="w-full flex justify-center">
+            <p className="">Hi, I'm Rithik</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-between area-content-2">
+          <SocialMediaLinks />
+          <DownButton className="h-16 cursor-pointer" />
+        </div>
       </div>
-      <img src={DownButton.src} className="h-20" />
     </div>
   );
 }
--- src/components/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks.tsx
@@ -17,11 +17,11 @@ const SocialMediaLinks: React.FC<SocialMediaLinks> = ({ className }) => {
             <a href="https://github.com/notrithik" target="_blank">
                 <GitHubIcon className="h-10" />
             </a>
-            <a href="https://t.me/notrithik" target="_blank">
-                <TelegramIcon className="h-10" />
-            </a>
             <a href="mailto:rithikum123@gmail.com" target="_blank">
                 <MailIcon className="h-10" />
+            </a>
+            <a href="https://t.me/notrithik" target="_blank">
+                <TelegramIcon className="h-10" />
             </a>
             <a href="https://x.com/proof_of_mind" target="_blank">
                 <XIcon className="h-10" />
